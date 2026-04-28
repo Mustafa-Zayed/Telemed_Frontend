a1017029a3607792c80331f2973729da5302bc27
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-04-28 19:05:34 +0300

    Footer & Home Page Created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav } from './nav/nav';
+import { Footer } from './footer/footer';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav],
+  imports: [RouterOutlet, Nav, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
